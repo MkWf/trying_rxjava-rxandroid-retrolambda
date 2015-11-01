674bbdf43212be52256b87c86faaded3f6c96ce0
Mark Wassef <markdevw@gmail.com>
2015-11-01 00:05:29 -0400

    Created simple observable that runs off a button click

--- app/src/main/java/com/gmail/markdevw/tryingrxjava/MainActivity.java
+++ app/src/main/java/com/gmail/markdevw/tryingrxjava/MainActivity.java
@@ -3,21 +3,36 @@ package com.gmail.markdevw.tryingrxjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
+
+import rx.Observable;
+import rx.Subscription;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     private Button button;
+    private Subscription subscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView) findViewById(R.id.tv_result);
+
         button = (Button) findViewById(R.id.btn);
         button.setOnClickListener(v -> runObservable());
     }
 
     private void runObservable() {
+        subscription = Observable.just("Hello")
+                .subscribe(s -> textView.setText(s));
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        subscription.unsubscribe();
     }
 }
