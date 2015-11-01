bc62466e5adb0f0c2783e94e1f0c9991c039cd7a
Mark Wassef <markdevw@gmail.com>
2015-11-01 16:47:25 -0500

    Added RxBinding library. Converted button clicklistener to RxView

--- app/src/main/java/com/gmail/markdevw/tryingrxjava/MainActivity.java
+++ app/src/main/java/com/gmail/markdevw/tryingrxjava/MainActivity.java
@@ -5,34 +5,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding.view.RxView;
+
 import rx.Observable;
 import rx.Subscription;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
-    private Button button;
-    private Subscription subscription;
+    private TextView mTextView;
+    private Button mButton;
+    private Subscription mSubscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.tv_result);
+        mTextView = (TextView) findViewById(R.id.tv_result);
 
-        button = (Button) findViewById(R.id.btn);
-        button.setOnClickListener(v -> runObservable());
+        mButton = (Button) findViewById(R.id.btn);
+        mSubscription = RxView.clicks(mButton)
+                .subscribe(s -> runObservable());
     }
 
     private void runObservable() {
-        subscription = Observable.just("Hello")
-                .subscribe(s -> textView.setText(s));
+        mSubscription = Observable.just("Hello")
+                .subscribe(s -> mTextView.setText(s));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        subscription.unsubscribe();
+        mSubscription.unsubscribe();
     }
 }
