711a1303b60c4eaacb3914665743acc27aa25baa
Mark Wassef <markdevw@gmail.com>
2015-11-01 19:03:28 -0500

    Added a login layout

--- app/src/main/java/com/gmail/markdevw/tryingrxjava/MainActivity.java
+++ app/src/main/java/com/gmail/markdevw/tryingrxjava/MainActivity.java
@@ -3,16 +3,14 @@ package com.gmail.markdevw.tryingrxjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
-import com.jakewharton.rxbinding.view.RxView;
-
-import rx.Observable;
 import rx.Subscription;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+    private EditText mUsername;
+    private EditText mPassword;
     private Button mButton;
     private Subscription mSubscription;
 
@@ -21,17 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.tv_result);
+        mUsername = (EditText) findViewById(R.id.et_username);
+        mPassword = (EditText) findViewById(R.id.et_password);
 
-        mButton = (Button) findViewById(R.id.btn);
-        mSubscription = RxView.clicks(mButton)
-                .subscribe(s -> runObservable());
+//        mButton = (Button) findViewById(R.id.btn);
+//        mSubscription = RxView.clicks(mButton)
+//                .subscribe(s -> runObservable());
     }
 
-    private void runObservable() {
-        mSubscription = Observable.just("Hello")
-                .subscribe(s -> mTextView.setText(s));
-    }
+//    private void runObservable() {
+//        mSubscription = Observable.just("Hello")
+//                .subscribe(s -> mTextView.setText(s));
+//    }
 
     @Override
     protected void onDestroy() {
