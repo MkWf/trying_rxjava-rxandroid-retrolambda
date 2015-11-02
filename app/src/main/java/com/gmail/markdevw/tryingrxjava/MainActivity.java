d09b6dae19831791d6a5fd58171098ff020ff1b4
Mark Wassef <markdevw@gmail.com>
2015-11-01 20:20:37 -0500

    Added ability to track when user was able to login

--- app/src/main/java/com/gmail/markdevw/tryingrxjava/MainActivity.java
+++ app/src/main/java/com/gmail/markdevw/tryingrxjava/MainActivity.java
@@ -1,10 +1,15 @@
 package com.gmail.markdevw.tryingrxjava;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.jakewharton.rxbinding.view.RxView;
+import com.jakewharton.rxbinding.widget.RxTextView;
+
+import rx.Observable;
 import rx.Subscription;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,18 +24,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mButton = (Button) findViewById(R.id.btn);
+        mButton.setEnabled(false);
+
+        RxView.clicks(mButton)
+                .subscribe(s -> startNextActivity());
+
         mUsername = (EditText) findViewById(R.id.et_username);
         mPassword = (EditText) findViewById(R.id.et_password);
 
-//        mButton = (Button) findViewById(R.id.btn);
-//        mSubscription = RxView.clicks(mButton)
-//                .subscribe(s -> runObservable());
+        initLoginObserver();
     }
 
-//    private void runObservable() {
-//        mSubscription = Observable.just("Hello")
-//                .subscribe(s -> mTextView.setText(s));
-//    }
+    private void initLoginObserver() {
+        Observable<CharSequence> obsUsername = RxTextView.textChanges(mUsername);
+        Observable<CharSequence> obsPassword = RxTextView.textChanges(mPassword);
+
+        mSubscription = Observable.combineLatest(obsUsername, obsPassword, (a, b) -> {
+            return a.length() > 5 && b.length() > 5;
+        })
+                .subscribe(o -> mButton.setEnabled(o));
+    }
+
+    private void startNextActivity() {
+        Intent nextActivity = new Intent(this, SecondActivity.class);
+        startActivity(nextActivity);
+    }
 
     @Override
     protected void onDestroy() {
